0abe985a4d26321d94265fa6b4ff5ffdda8d6446
argeento <adrian.wieprzkowicz@gmail.com>
2022-06-11 00:21:01 +0200

    Add index html WIP

---
+++ src/components/MyCropper.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Cropper from 'cropperjs';
+  import { onMount } from 'svelte';
+  import 'cropperjs/dist/cropper.min.css';
+
+  onMount(() => {
+    const image = document.querySelector<HTMLImageElement>('#image')!;
+    const cropper = new Cropper(image, {
+      aspectRatio: 16 / 9,
+      crop(event) {
+        console.log(event.detail.x);
+        console.log(event.detail.y);
+        console.log(event.detail.width);
+        console.log(event.detail.height);
+        console.log(event.detail.rotate);
+        console.log(event.detail.scaleX);
+        console.log(event.detail.scaleY);
+      },
+    });
+  });
+</script>
+
+<div class="cropper-wrap">
+  <img id="image" class="img" alt="" src="raven.jpg" />
+</div>
+
+<style>
+  .cropper-wrap {
+    width: 500px;
+    height: 500px;
+  }
+  .img {
+    display: block;
+    max-width: 100%;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,11 @@
-<h1 class="text-3xl font-bold">Welcome to <span class="text-blue-600">overlay.link</span>!</h1>
+<script lang="ts">
+  import MyCropper from '../components/MyCropper.svelte';
+</script>
+
+<h1 class="text-3xl font-bold text-center mb-10">
+  Welcome to <span class="text-blue-600">overlay.link!</span>
+</h1>
+
+<p class="text-center">Please upload your photo</p>
+
+<!-- <MyCropper /> -->
